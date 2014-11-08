9106a5ade3964a660897739aae3fefd1a16fe585
Thawon Uttamavanit <uttamavanit@gmail.com>
2014-11-08 22:05:41 +1100

    Remove session service

--- app/scripts/services/signup-service.js
+++ app/scripts/services/signup-service.js
@@ -1,22 +1,22 @@
 ﻿define(
-    ["app", "angular", "services/session-service"],
+    ["app", "angular"],
     function (app, angular) {
-        app.factory("SignupService", ["$http", "Session",
-            function ($http, Session) {
+        app.factory("SignupService", ["$http",
+            function ($http) {
                 var signupService = {};
 
                 signupService.signup = function (credentials) {
-                    return $http.post("/signup", credentials)
-                                .success(function (data, status, headers, config) {
-                                    Session.create(res.data.id, res.data.user.id,
-                                                       res.data.user.role);
+//                    return $http.post("/signup", credentials)
+//                                .success(function (data, status, headers, config) {
+//                                    Session.create(res.data.id, res.data.user.id,
+//                                                       res.data.user.role);
 
-                                    return res.data.user;
-                                })
-                                .error(function (data, status, headers, config) {
-                                    var x;
-                                    x = 1;
-                                });
+//                                    return res.data.user;
+//                                })
+//                                .error(function (data, status, headers, config) {
+//                                    var x;
+//                                    x = 1;
+//                                });
                 };
 
                 return signupService;
